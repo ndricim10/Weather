e3f002af757646878929575e97cb1a170b83b215
Ndricim Jahaj <jahajndricim@gmail.com>
2022-05-05 09:26:09 +0300

    render function

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-// Selectors
 const searchButton = document.querySelector('[data-location]')
 const inputSearch = document.querySelector('[data-city]')
 const back = document.querySelector('.back')
 const temp = document.querySelector('.temp')
 const clouds = document.querySelector('.clouds')
 const location1 = document.querySelector('.location')
+const spanDisplay = document.querySelector('[data-span-display]')
+const weatherDisplay = document.querySelector('[data-weather-display]')
 
 // Weather API
 const apiKey = `63c8878e4f9fbdea6f5667f42f9913c9`
@@ -14,10 +15,50 @@ inputSearch.addEventListener('keyup', e => {
     e = e.key;
     if (e == 'Enter' && inputSearch.value != "") {
         requestApi(inputSearch.value)
+        weatherDisplay.setAttribute('data-weather-display', 'true')  
+        spanDisplay.setAttribute('data-span-display', 'false')
+        inputSearch.value=''
+    }
+    else{
+        spanDisplay.setAttribute('data-span-display', 'true')
     }
     // weatherDetails() 
 })
 
+searchButton.addEventListener('click', ()=>{
+    if(inputSearch.value != ""){
+        requestApi(inputSearch.value)
+        weatherDisplay.setAttribute('data-weather-display', 'true')
+        spanDisplay.setAttribute('data-span-display', 'false')
+        inputSearch.value=''
+    }
+    else{
+        spanDisplay.setAttribute('data-span-display', 'true')
+    }
+})
+
+back.addEventListener('click', ()=>{
+    weatherDisplay.setAttribute('data-weather-display', 'false')
+})
+
+function render(){
+    apiLink = `https://api.openweathermap.org/data/2.5/weather?q=Athens&appid=${apiKey}`
+    fetch(apiLink)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            temp.innerHTML=KelvinToCelcius(data.main.temp) + '°C'
+            clouds.innerHTML=data.weather[0].description
+            location1.innerHTML = data.name + ', ' + data.sys.country
+            document.querySelector('.feels').innerHTML=KelvinToCelcius(data.main.feels_like) + '°C'
+            document.querySelector('.span-min').innerHTML=KelvinToCelcius(data.main.temp_min) + '°C'
+            document.querySelector('.span-max').innerHTML=KelvinToCelcius(data.main.temp_max) + '°C'
+            document.querySelector('.speed-wind').innerHTML=data.wind.speed
+            inputSearch.value=''
+        })
+
+    }
+    render()
 
 function requestApi(city) {
     apiLink = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
@@ -27,12 +68,17 @@ function requestApi(city) {
 
 }
 
-function weatherDetails(info) {
+function weatherDetails(data) {
     // KelvinToCelcius(info.main.feels_like)
-    console.log(info);
-    temp.innerHTML=KelvinToCelcius(info.main.temp) + '°C'
-    clouds.innerHTML=info.weather[0].description
-    location1.innerHTML = info.name + ', ' + info.sys.country;
+    console.log(data);
+    temp.innerHTML=KelvinToCelcius(data.main.temp) + '°C'
+    clouds.innerHTML=data.weather[0].description
+    location1.innerHTML = data.name + ', ' + data.sys.country
+    document.querySelector('.feels').innerHTML=KelvinToCelcius(data.main.feels_like) + '°C'
+    document.querySelector('.span-min').innerHTML=KelvinToCelcius(data.main.temp_min) + '°C'
+    document.querySelector('.span-max').innerHTML=KelvinToCelcius(data.main.temp_max) + '°C'
+    document.querySelector('.speed-wind').innerHTML=data.wind.speed
+    inputSearch.value=''
 }
 
 let celcius;
